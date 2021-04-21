8c51d3f6b6a70f5067bef37bb470fde0ac2d7534
GalenGrieman <40723071+GalenGrieman@users.noreply.github.com>
2021-04-21 14:08:52 -0500

    Add files via upload

---
+++ LINQEntityFrameWork01/CarDb.cs
@@ -0,0 +1,17 @@
+﻿using Cars;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQEntityFramework
+{
+    //This is a class representation of our Database
+    //Properties represent tables on the database
+    public class CarDb : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
